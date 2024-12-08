51f6d2b6b7cbfeb52b35fc5fec82b6b9895d9c27
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2024-12-08 22:30:44 +0200

    update 2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import { Application, Session } from "./deps.js";
-import { authMiddleware } from "./middlewares/authMiddleware.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import { renderMiddleware } from "./middlewares/renderMiddleware.js";
-import { userMiddleware } from "./middlewares/userMiddleware.js";
-import { router } from "./routes/routes.js";
+import { Application, Session } from "./app/deps.js";
+import { authMiddleware } from "./app/middlewares/authMiddleware.js";
+import { errorMiddleware } from "./app/middlewares/errorMiddleware.js";
+import { renderMiddleware } from "./app/middlewares/renderMiddleware.js";
+import { userMiddleware } from "./app/middlewares/userMiddleware.js";
+import { router } from "./app/routes/routes.js";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const app = new Application();
